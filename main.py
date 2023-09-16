17cf0bff9adba89670c60a18587ab93ea70b1c1e
Hasan3773 <Hasan.tahir3773@gmail.com>
2023-09-16 16:24:17 -0400

    Implemented some quaternion calculations

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 ''' Demonstrates how to subscribe to and handle data from gaze and event streams '''
 
+
 import time
 
 import adhawkapi
 import adhawkapi.frontend
 from pyquaternion import Quaternion
 import matplotlib.pyplot as plt
+import numpy
 
 class FrontendData:
     ''' BLE Frontend '''
@@ -15,29 +17,39 @@ class FrontendData:
         # TODO: Update the device name to match your device
         self._api = adhawkapi.frontend.FrontendApi(ble_device_name='ADHAWK MINDLINK-304')
 
+
         # Tell the api that we wish to receive eye tracking data stream
         # with self._handle_et_data as the handler
         self._api.register_stream_handler(adhawkapi.PacketType.EYETRACKING_STREAM, self._handle_et_data)
 
+
         # Tell the api that we wish to tap into the EVENTS stream
         # with self._handle_events as the handler
         self._api.register_stream_handler(adhawkapi.PacketType.EVENTS, self._handle_events)
+
 
         # Start the api and set its connection callback to self._handle_tracker_connect/disconnect.
         # When the api detects a connection to a MindLink, this function will be run.
         self._api.start(tracker_connect_cb=self._handle_tracker_connect,
                         tracker_disconnect_cb=self._handle_tracker_disconnect)
 
+
     def shutdown(self):
         '''Shutdown the api and terminate the bluetooth connection'''
         self._api.shutdown()
 
+
     @staticmethod
     def _handle_et_data(et_data: adhawkapi.EyeTrackingStreamData):
         ''' Handles the latest et data '''
+        uvec = None
+        mag = None
         if et_data.gaze is not None:
             xvec, yvec, zvec, vergence = et_data.gaze
+            mag = math.sqr(xvec**2, yvec**2, zvec**2)
+            uvec = numpy.array([xvec/mag, yvec/mag, zvec/mag])
             print(f'Gaze={xvec:.2f},y={yvec:.2f},z={zvec:.2f},vergence={vergence:.2f}')
+
 
         if et_data.eye_center is not None:
             if et_data.eye_mask == adhawkapi.EyeMask.BINOCULAR:
@@ -45,15 +57,30 @@ class FrontendData:
                 print(f'Eye center: Left=(x={lxvec:.2f},y={lyvec:.2f},z={lzvec:.2f}) '
                       f'Right=(x={rxvec:.2f},y={ryvec:.2f},z={rzvec:.2f})')
 
+
         if et_data.pupil_diameter is not None:
             if et_data.eye_mask == adhawkapi.EyeMask.BINOCULAR:
                 rdiameter, ldiameter = et_data.pupil_diameter
                 print(f'Pupil diameter: Left={ldiameter:.2f} Right={rdiameter:.2f}')
 
+        rotator = None
         if et_data.imu_quaternion is not None:
             if et_data.eye_mask == adhawkapi.EyeMask.BINOCULAR:
                 x, y, z, w = et_data.imu_quaternion
+                rotator = Quaternion(axis=[x, y, z], angle=w)
                 print(f'IMU: x={x:.2f},y={y:.2f},z={z:.2f},w={w:.2f}')
+
+
+        print("--------------------")
+
+
+        if rotator and uvec and mag:
+            absvec = rotator.rotate(uvec)
+            absvec = numpy.array([absvec[0] * mag, absvec[1] * mag, absvec[2] * mag])
+
+
+        print("--------------------")
+
 
     @staticmethod
     def _handle_events(event_type, timestamp, *args):
@@ -67,9 +94,11 @@ class FrontendData:
             eye_idx = args[0]
             print(f'Eye Open: {timestamp} {eye_idx}')
 
+
     def _handle_tracker_connect(self):
         print("Tracker connected")
         self._api.set_et_stream_rate(60, callback=lambda *args: None)
+
 
         self._api.set_et_stream_control([
             adhawkapi.EyeTrackingStreamTypes.GAZE,
@@ -78,13 +107,15 @@ class FrontendData:
             adhawkapi.EyeTrackingStreamTypes.IMU_QUATERNION,
         ], True, callback=lambda *args: None)
 
+
         self._api.set_event_control(adhawkapi.EventControlBit.BLINK, 1, callback=lambda *args: None)
         self._api.set_event_control(adhawkapi.EventControlBit.EYE_CLOSE_OPEN, 1, callback=lambda *args: None)
+
 
     def _handle_tracker_disconnect(self):
         print("Tracker disconnected")
 
-    # Matplot lib func
+   # Matplot lib func
     def _matplotlib_func(x, y):
         fig, ax = plt.sublots()
         ax.plot([0,10,10,0,0], [0, 0, 10, 10, 0])
@@ -106,6 +137,7 @@ def main():
             time.sleep(1)
     except (KeyboardInterrupt, SystemExit):
         frontend.shutdown()
+
 
 if __name__ == '__main__':
     main()
